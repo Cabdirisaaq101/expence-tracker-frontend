802f8d979b2c772e1f8141e4784ccf302926c3ca
Cabdirisaaq101 <crisaaq210@gmail.com>
2025-07-16 15:37:55 +0300

    updated the axios.ts

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://expense-api.up.railway.app",
+  baseURL: "https://railway.com/project/b607a779-3504-46c4-a524-9980a9e2dfa3/service/9cd080ff-3b61-4236-a7a7-1f9b60ef15ad?environmentId=b443074b-cb95-4650-a96a-c3187f067570",
   withCredentials: true,
 });
 
