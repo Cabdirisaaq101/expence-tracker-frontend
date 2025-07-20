6aa4e5889c67d3a2c237c1a8ed098b24e0729308
Cabdirisaaq101 <crisaaq210@gmail.com>
2025-07-20 14:36:00 +0300

    update axios baseURL for Railway

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,9 +1,8 @@
-// src/api/axios.ts
 import axios from "axios";
 
-const instance = axios.create({
-  baseURL: "https://railway.com/project/b607a779-3504-46c4-a524-9980a9e2dfa3/service/9cd080ff-3b61-4236-a7a7-1f9b60ef15ad?environmentId=b443074b-cb95-4650-a96a-c3187f067570",
-  withCredentials: true,
+const api = axios.create({
+  baseURL: "https://expense-tracker-backend-production-07c9.up.railway.app/api",
+  withCredentials: true, // Optional if you're using cookies or auth
 });
 
-export default instance;
+export default api;
