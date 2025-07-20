d81520b4eddb5c6dd47ca93fbf1ab4bf3fcc649a
Cabdirisaaq101 <crisaaq210@gmail.com>
2025-07-20 14:48:05 +0300

    fix: update CORS for Vercel frontend

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://expense-tracker-backend-production-07c9.up.railway.app/api",
-  withCredentials: true, // Optional if you're using cookies or auth
+  baseURL: "https://expense-tracker-backend-production-07c9.up.railway.app",
+  withCredentials: true,
 });
 
 export default api;
